c3d45f492d5486f7073d3589dee8c39fa759f65b
Sérgio Moreira <sergio100br@hotmail.com>
2016-09-13 23:42:59 -0300

    commit inicial

---
+++ lista.php
@@ -0,0 +1,44 @@
+<?php
+
+$pdo = new PDO('mysql:dbname=testes;host=127.0.0.1','root','09271185');
+
+$rs = $pdo->query("SELECT * FROM menu where codpai is null");
+$rs->setFetchMode(PDO::FETCH_CLASS, 'MenuItem');
+
+while($row = $rs->fetch()){
+	//echo $row['nome'];
+}
+ 
+
+
+
+function criarMenu($categoriaPai) {
+echo "<ul>";
+
+// Parte-1
+// Seleciona os registros do banco
+//$categorias = new ProdutoCategoria();
+//$where = "categoriaPai =".$categoriaPai;
+//$categorias = $categorias->select($where);
+global $pdo;
+if(!$categoriaPai){$sql = "SELECT * FROM menu where codpai is NULL";}else{
+$sql = "SELECT * FROM menu where codpai = $categoriaPai";
+}
+//echo "SELECT * FROM menu where codpai = $categoriaPai";
+$rs = $pdo->query($sql);
+$rs->setFetchMode(PDO::FETCH_CLASS, 'MenuItem');
+
+while($row = $rs->fetch()){
+ 
+	echo "<li>".$row["nome"];
+	criarMenu($row["codmenu"]); // recursividade
+		echo "</li>";
+	}
+	echo "</ul>";
+}
+
+criarMenu(0);
+?>
+
+
+
